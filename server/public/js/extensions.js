cbf2dbe0b8d82fbea25e720f822bb8019dba2fe9
Pablo Fernandez <fernandezpablo85@gmail.com>
2011-01-21 19:05:01 -0300

    That's just how I roll

--- server/public/js/extensions.js
+++ server/public/js/extensions.js
@@ -5,7 +5,33 @@ Extensions.onLoad = function () {
 }
 
 Extensions.onAuth = function () {
-document.getElementById('link').innerText = "document.location.href";
-document.getElementById('title').value = "document.title";
-document.getElementById('share-link').style.display = 'block';
+	document.getElementById('link').innerText = "http://example.com";
+	document.getElementById('title').value = "document.title";
+	document.getElementById('share-link').style.display = 'block';
+}
+
+Extensions.share = function() {
+	document.getElementById('submit').style.disabled = true;
+	var link = document.getElementById('link').innerText;
+	var title = document.getElementById('title').value;
+	var comment = document.getElementById('comment').value;
+	var post_body =  { "content": { "submitted-url": link, "title": title}, "visibility": {"code": "anyone"}, "comment": comment };
+
+	IN.API.Raw("/people/~/shares")
+		.method("POST")
+		.body(JSON.stringify(post_body))
+		.result(Extensions.callbacks.shareOk)
+		.error(Extensions.callbacks.shareError);
+}
+
+Extensions.callbacks = {};
+
+Extensions.callbacks.shareOk = function (response) {
+	document.getElementById('share-link').style.display = 'none';
+	document.getElementById('messages').innerText = 'Link shared! :)';
+}
+
+Extensions.callbacks.shareError = function (response) {
+	document.getElementById('share-link').style.display = 'none';
+	document.getElementById('messages').innerText = 'There were problems sharing this link. Try again later :(';
 }
