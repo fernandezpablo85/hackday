6b3db6dd4a3be668c44451f93bb6d8542732d54b
Alejandro Bologna <abologna@apples-macbook-pro.local>
2011-01-23 15:13:23 -0300

    show linkedin user name - sign out link action

--- server/public/js/extensions.js
+++ server/public/js/extensions.js
@@ -4,13 +4,15 @@ Extensions.onLoad = function () {
 	IN.Event.on(IN, 'auth', Extensions.onAuth);
 }
 
-Extensions.onAuth = function () {
+Extensions.onAuth = function (result) {
+	console.log(result.toString());
 	var matches = location.search.match(/url=(.*)&title=(.*)/);
 	var fullUrl = decodeURIComponent(matches[1]);
 	document.getElementById('link').innerText = fullUrl;
 	document.getElementById('title').value = decodeURIComponent(matches[2]);
 	document.getElementById('share-link').style.display = 'block';
-	document.getElementById('link-display').innerText = fullUrl.substring(0,30) + "..."
+	document.getElementById('link-display').innerText = fullUrl.substring(0,30) + "...";
+	IN.API.Profile("me").result(Extensions.callbacks.getProfile);
 }
 
 Extensions.share = function() {
@@ -31,6 +33,12 @@ Extensions.closePopup = function () {
 	parent.close();	
 }
 
+Extensions.logout = function() {
+	document.getElementById('share-link').style.display = 'none';
+	document.getElementById('header').style.display = 'none';
+	IN.User.logout();
+}
+
 Extensions.callbacks = {};
 
 Extensions.callbacks.shareOk = function (response) {
@@ -43,4 +51,8 @@ Extensions.callbacks.shareError = function (response) {
 	setTimeout("parent.close()", 2000);
 	document.getElementById('share-link').style.display = 'none';
 	document.getElementById('messages').innerHTML = '<p>Could not share link</p>';
+}
+
+Extensions.callbacks.getProfile = function (response) {
+	document.getElementById('username').innerText = response.values[0].firstName + ' ' + response.values[0].lastName;
 }
